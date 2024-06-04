70d00888477fd75294a8d7d6014bb8c221dcfce4
singh <singhkailash2001@gmail.com>
2024-06-04 14:44:11 +0530

    slice function

--- Calculator codsoft/script.js
+++ Calculator codsoft/script.js
@@ -1,14 +1,16 @@
-let btn = document.querySelectorAll("button");
+let  btn= document.querySelectorAll("button");
       for (let i of btn) {
         i.onclick = function () {
+          let a = document.querySelector("input").value;
           if (i.innerText == "=") {
-            let a = document.querySelector("input").value;
+           
 
             document.querySelector("input").value = eval(a);
-          } else if (i.innerText == "Ac") {
+          }
+           else if (i.innerText == "Ac") {
             document.querySelector("input").value = " ";
-          } else if (i.innerText == "c") {
-            document.querySelector("input").value = slice(0, -1);
+          } else if (i.innerText === "c") {
+              document.querySelector('input').value =slice(0,-1);
           } else {
             document.querySelector("input").value += i.innerText;
           }
