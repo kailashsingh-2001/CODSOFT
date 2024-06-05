08b60d0eed3eb22e6edcca1646a1bf9644c6b067
singh <singhkailash2001@gmail.com>
2024-06-05 17:34:48 +0530

    landing page

---
+++ landinghpage/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+function click(){
+    var elements = document.getElementsByClassName("openr");
+    for(var i = 0; i < elements.length; i++){
+        elements[i].style.display = "block";
+    }
+}
